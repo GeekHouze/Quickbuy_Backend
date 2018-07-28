34232b3771eec616369b1adbf23de600f4a384da
Nhlanhladev <dhlamininhlanhla1@gmail.com>
2018-07-28 21:29:48 +0200

    new features added

--- src/main/java/com/geekhouze/stockdb_spring_backend/resource/ServerResource.java
+++ src/main/java/com/geekhouze/stockdb_spring_backend/resource/ServerResource.java
@@ -4,10 +4,7 @@ package com.geekhouze.stockdb_spring_backend.resource;
 import com.geekhouze.stockdb_spring_backend.model.Server;
 import com.geekhouze.stockdb_spring_backend.repository.ServerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +16,11 @@ public class ServerResource {
     private ServerRepository serverRepository;
 
 
-    @GetMapping(name ="/")
+    @GetMapping(value ="/")
     public List<Server> getServer(){
         return serverRepository.findAll();
     }
-
+    @PostMapping(value ="/")
     public List<Server>addServer(@RequestBody Server server){
         serverRepository.save(server);
         return serverRepository.findAll();
